a957c1a996cfae0d74621f707c8d310960516656
goal-eng <goooaldev@gmail.com>
2025-01-23 03:18:03 +0900

    Add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './assets/style/global.scss';
+import './assets/style/index.css';
 
 import { Routers } from './routers';
 import { Provider } from './provider';
