d5ce5c1cde82e2ed0cba7da9e0ded9a589e659b1
MrLad01 <ladayodeji@gmail.com>
2025-02-01 22:27:31 -0800

    update on contact us page

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -10,15 +10,15 @@ import { NavLink } from 'react-router-dom';
 export default function ContactUs() {
   return (
     <div className='w-full dm-sans'>
-        <div className="w-full h-screen bg-[#F6F6F6] rounded-b-[50px] relative" >
+        <div className="w-full h-screen max-lg:h-full bg-[#F6F6F6] rounded-b-[50px] relative" >
             <div className="absolute top-10 right-8 z-10">
-                <img src={heroBackground} alt="" className=' h-[600px] w-[880px] object-fill' />
+                <img src={heroBackground} alt="" className=' h-[600px]  w-[880px] max-sm:w-[320px] object-fill' />
             </div>
             <Navbar /> 
-            <main className='w-full h-[88%] px-32 py-6'>
-                <div className="w-full h-full flex justify-between items-center relative z-20">
-                    <div className="w-[500px] h-[400px] flex flex-col space-y-6">
-                        <span className='text-[54px]/16 dm-sans-medium'>Contact Us</span>
+            <main className='w-full h-[88%] px-32 max-sm:pt-12 max-sm:px-4 max-lg:px-20 py-6'>
+                <div className="w-full h-full flex max-lg:grid max-lg:gap-4 justify-between items-center relative z-20">
+                    <div className="w-[500px] max-sm:w-[320px] max-lg:w-full h-[400px] flex flex-col space-y-6 max-sm:space-y-4">
+                        <span className='text-[54px]/16 max-sm:text-[34px]/10 dm-sans-medium'>Contact Us</span>
                         <p className='text-[#6E757D] dm-sans-light text-[14px]/6'>Get in touch with us for any questions, support, or feedback.<br /> Our team is ready to assist you every step of the way!</p>
                         <div className='flex flex-col space-y-2'>
                             <div className="flex space-x-3 items-center">
@@ -42,7 +42,7 @@ export default function ContactUs() {
                         </div>
                         <p className='text-[#6E757D] dm-sans-light text-[14px]/6'>Looking for careers? <NavLink to={``} className='text-[#2475FF]'>View all job openings.</NavLink></p>
                     </div>
-                    <div className="w-[480px] flex items-center h-full">
+                    <div className="w-[480px] max-sm:w-[340px] flex items-center h-full">
                         <div className="w-[440px] h-full p-4 rounded-2xl shadow-2xl bg-white">
                             <div className="w-full h-full flex flex-col space-y-1">
                                 <span className='text-[26px] dm-sans-medium'>Get in Touch</span>
@@ -86,44 +86,44 @@ export default function ContactUs() {
                 </div>
             </main>
         </div>
-        <div className="w-full bg-white pt-24 px-12 space-y-14" >
-            <div className="w-full px-20 flex flex-col items-center">
+        <div className="w-full bg-white pt-24 px-12 max-lg:px-3 space-y-14 max-sm:space-y-6" >
+            <div className="w-full px-20 max-lg:px-3 flex flex-col items-center">
                 <div className='w-full flex flex-col items-center space-y-3'>
                     <span className='text-[36px] dm-sans-medium'>Visit us</span>
                     <div className='flex flex-col items-center space-y-1'>
                         <span className='text-[#6E757D] dm-sans-light text-center text-[14px]/6'>Home is behind, the world ahead and there are many paths to tread <br /> through shadows to the edge.</span>
                     </div>
-                    <div className="grid grid-cols-5 w-full h-[80px] gap-4">
-                        <div className='w-full h-full py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
+                    <div className="grid grid-cols-5 max-lg:grid-cols-3 max-sm:grid-cols-1 w-full h-[80px] max-lg:h-full gap-4">
+                        <div className='w-full h-full max-sm:h-[100px] py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
                             <img src={socialIcons.youtubeIcon} alt="youtube logo" className='w-[8rem] object-cover' />
                         </div>
-                        <div className='w-full h-full py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
+                        <div className='w-full h-full max-sm:h-[100px] py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
                             <img src={socialIcons.linkedInIcon} alt="linkedIn logo" className='w-[8rem] object-cover' />
                         </div>
-                        <div className='w-full h-full py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
+                        <div className='w-full h-full max-sm:h-[100px] py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
                             <img src={socialIcons.instagramIcon} alt="instagram logo" className='w-[8rem] object-cover' />
                         </div>
-                        <div className='w-full h-full py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
+                        <div className='w-full h-full max-sm:h-[100px] py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
                             <img src={socialIcons.twitterIcon} alt="twitter logo" className='w-[8rem] object-cover' />
                         </div>
-                        <div className='w-full h-full py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
+                        <div className='w-full h-full max-sm:h-[100px] py-3 bg-[#F6F6F6] border border-[#D3D8DD] rounded-3xl flex items-center justify-center'>
                             <img src={socialIcons.facebookIcon} alt="facebook logo" className='w-[8rem] object-cover' />
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="w-full px-20 flex flex-col items-center">
+            <div className="w-full px-20 max-lg:px-3 max-sm:h-[300px] flex flex-col items-center">
                 <img src={contactBack} alt="" className='w-full h-full' />
             </div>
-            <div className="w-full px-20 py-16 flex flex-col items-center space-y-8 bg-white">
-                <div className="w-full flex justify-between items-start">
+            <div className="w-full px-20 max-lg:px-3 py-16 max-sm:py-4 flex flex-col items-center space-y-8 bg-white">
+                <div className="w-full flex max-sm:flex-col justify-between items-start">
                     <h2 className="text-[36px] dm-sans-medium">Visit our offices around <br /> the world</h2>
                     <p className="text-[#6E757D] dm-sans-light text-[14px] max-w-[200px]">
                         A short paragraph describing your team. You may want to describe what they are responsible for
                     </p>
                 </div>
                 
-                <div className="grid grid-cols-3 gap-6 w-full">
+                <div className="grid grid-cols-3 max-sm:grid-cols-1 gap-6 max-sm:gap-3 w-full">
                     {/* Dubai Office */}
                     <div className="bg-[#F6F6F6] rounded-xl p-6 space-y-4">
                         <div className="flex items-center space-x-2">
