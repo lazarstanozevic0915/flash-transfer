f8b718419cb03044211f7ae7f9ec9d27606038d6
Oluwanifemi <ayodeletolu8@gmail.com>
2025-02-04 13:09:08 +0000

    update

--- flashmobile/app/(auth)/splash.jsx
+++ flashmobile/app/(auth)/splash.jsx
@@ -18,9 +18,9 @@ export default function SplashScreen() {
     <View className="flex-1 bg-white items-center justify-center p-12">
       <Image 
         source={logo}
-        className="w-32 h-32 mb-4 mt-12"
+        className="w-32 h-32 mb-4 mt-20"
       />
-      <Text className="text-2xl font-bold text-gray-900">Flash Transfer</Text>
+      <Text className="font-bold text-gray-800 mb-2 text-center text-[32px]">Flash Transfer</Text>
       <Text className="text-sm text-gray-600 mt-2">Your Best Money Transfer Partner</Text>
       <View className="flex-1 justify-end items-end p-4">
         <Text>
